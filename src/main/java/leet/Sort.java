5f999571aed948e15ce6ddf7b9d448eeb1236647
yiwen.gao <ygao@millennialmedia.com>
2015-12-30 11:22:22 -0500

    add

---
+++ src/main/java/leet/Sort.java
@@ -0,0 +1,14 @@
+package leet;
+
+/**
+ * Created by yiwengao on 12/30/15.
+ */
+public class Sort {
+
+    public int median(int[] nums) {
+        // write your code here
+
+
+
+    }
+}
--- src/main/java/leet/SortedArray.java
+++ src/main/java/leet/SortedArray.java
@@ -1,13 +1,42 @@
 package leet;
 
+import java.util.ArrayList;
+
 /**
  * Created by yiwengao on 12/27/15.
  */
 public class SortedArray {
 
 
-    public void mergeSortedArray(int[] A, int m, int[] B, int n) {
-        // write your code here
 
+
+    public void recoverRotatedSortedArray(ArrayList<Integer> nums) {
+        // write your code
+         if (nums.size() <2){
+             return;
+         }
+         int cur = nums.get(0);
+        for (int i=1; i <nums.size() ; i++){
+            if (cur > nums.get(i)){
+                int pos = i-1;
+                while(pos<nums.size()-1) {
+                    shift(pos, nums);
+                    pos++;
+                }
+
+
+
+            }
+            cur = nums.get(i);
+        }
+    }
+
+    private void shift(int c, ArrayList<Integer> nums ) {
+         int last = nums.get(nums.size()-1);
+        for (int i= nums.size()-2 ; i >=0 ;i--) {
+            nums.set(i+1, nums.get(i));
+
+        }
+        nums.set(0, last);
     }
 }
